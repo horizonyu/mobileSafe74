47c7671fb0a9cf997616513aaace717982e08c8f
root <1501671995@qq.com>
2017-01-23 22:36:34 +0800

    第三次提交——成功打开了数据库中data1表，接下来应该做的是根据data1表中的外键outkey从data2表中查询出手机号的具体归属地。

--- src/com/itheima/mobilesafe74/engine/AddressDao.java
+++ src/com/itheima/mobilesafe74/engine/AddressDao.java
@@ -8,19 +8,19 @@ import android.util.Log;
  * 工具类，用于进行手机号归属地的查询
  * Created by horizon on 1/23/2017.
  */
+//TODO 成功打开数据库并进行data1表的数据查询，接下来需要做的是通过data1表中的外键outkey从data2中找到对应的手机号的归属地。
 
 public  class AddressDao {
     //1. 设置数据库的路径
-    static String path = "data/data/com.itheima.mobilesafe74/address.db";
+    static String path = "data/data/com.itheima.mobilesafe74/files/address.db";
 
-    //2. 打开数据库
-   // SQLiteDatabase db = SQLiteDatabase.openDatabase(path,null,SQLiteDatabase.OPEN_READWRITE);
-    static SQLiteDatabase db = SQLiteDatabase.openDatabase(path,null,SQLiteDatabase.OPEN_READONLY);
-
-    //3. 进行查询
+    //2. 进行查询
     public  static void getAddress(String phone){
         phone = phone.substring(0,7);
-        Cursor cursor = db.query("table1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
+
+         //3. 打开数据库
+          SQLiteDatabase db = SQLiteDatabase.openDatabase(path,null,SQLiteDatabase.OPEN_READONLY);
+          Cursor cursor = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
 
         //3.1 如果存在数据则停止查询
         if(cursor.moveToNext()){
