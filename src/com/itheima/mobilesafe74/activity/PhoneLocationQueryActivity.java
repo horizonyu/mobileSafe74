91644b464da9d7e216e9b3fd2c27638e11589e34
root <1501671995@qq.com>
2017-01-24 14:39:13 +0800

    第四次提交——实现从数据库中根据手机号码获取手机归属地信息，但对于输入的手机号未作规范，所以接下来需要做的是对输入的手机号码通过正则表达式进行规范。

--- src/com/itheima/mobilesafe74/activity/PhoneLocationQueryActivity.java
+++ src/com/itheima/mobilesafe74/activity/PhoneLocationQueryActivity.java
@@ -2,6 +2,7 @@ package com.itheima.mobilesafe74.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,27 +18,36 @@ public class PhoneLocationQueryActivity extends Activity {
     private EditText et_phone;
     private  Button bt_location_query;
     private TextView tv_location_display;
+    private String phone_number = "";
+    private String location = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_query);
 
-        //初始化UI界面
+        //1. 初始化UI界面
         initUI();
 
-        //执行查询方法
-        queryLocation("13000001234");
+        //2. 当点击查询按钮时执行查询方法
+        bt_location_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                queryLocation();
+            }
+        });
+
 
     }
 
     private void initUI() {
-        //输入需要查询的手机号
+        //1.1 输入需要查询的手机号
         et_phone = (EditText) findViewById(R.id.et_phone_query);
-
         bt_location_query = (Button) findViewById(R.id.bt_query_location);
 
-        //显示归属地信息
-        tv_location_display = (TextView) findViewById(R.id.tv_query_phoneNumber_location );
+        //1.2 显示归属地信息
+        tv_location_display = (TextView) findViewById(R.id.tv_location_display );
+
 
         //获取输入的手机号
         //    String phone_number = et_phone.getText().toString();
@@ -45,9 +55,21 @@ public class PhoneLocationQueryActivity extends Activity {
     }
 
 
-    public  void queryLocation(final String phone_number){
+    public  void queryLocation(){
+        //""
+        //2.1 获取输入的手机号
+        phone_number = et_phone.getText().toString();
 
-        AddressDao.getAddress(phone_number);
+        //2.2 获取具体的归属地信息
+        if(phone_number != ""){
+            location = AddressDao.getAddress(phone_number);
+        }
+
+        //2.3 显示具体的归属地信息
+        if(location != ""){
+            tv_location_display.setText(location);
+        }
+
         /*
         final String number = phone_number.substring(0,7);
 
--- src/com/itheima/mobilesafe74/engine/AddressDao.java
+++ src/com/itheima/mobilesafe74/engine/AddressDao.java
@@ -11,26 +11,49 @@ import android.util.Log;
 //TODO 成功打开数据库并进行data1表的数据查询，接下来需要做的是通过data1表中的外键outkey从data2中找到对应的手机号的归属地。
 
 public  class AddressDao {
+
     //1. 设置数据库的路径
     static String path = "data/data/com.itheima.mobilesafe74/files/address.db";
 
     //2. 进行查询
-    public  static void getAddress(String phone){
-        phone = phone.substring(0,7);
+    public  static String getAddress(String phone){
+        //0. 用于显示手机的归属地信息
+        String location = null;
+        if(phone != null){
+            if(phone.length() == 11){
+                phone = phone.substring(0,7);
+            }else if(phone.length() > 12 || phone.length() < 3 ){
+                return "未知号码";
+            }
+            else{
+
+            }
+
+        }
+
 
          //3. 打开数据库
           SQLiteDatabase db = SQLiteDatabase.openDatabase(path,null,SQLiteDatabase.OPEN_READONLY);
-          Cursor cursor = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
 
-        //3.1 如果存在数据则停止查询
-        if(cursor.moveToNext()){
-            String outkey = cursor.getString(0);
+        //3.1 首先查询data1表
+        Cursor data1 = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
 
+        if(data1.moveToNext()){
+            String outkey = data1.getString(0);
             Log.i("outkey",outkey);
-        }
-    }
-    //3. 如果数据库中存在数据，则进行查询
 
+            //3.2 接着通过外键查询data2
+            Cursor data2 = db.query("data2", new String[]{"location"}, "id = ?", new String[]{outkey}, null, null, null);
+
+            if(data2.moveToNext()){
+               location = data2.getString(0);
+                Log.i("location: ", location);
+            }
+
+        }
+
+        return location;
+    }
 
 
 
